2959b885261fd65c5e2f07edb99a525252a8b775
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-27 14:49:57 -0500

    Add specialization getter to teacher

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
+  attr_reader :specialization
+
   def initialize(specialization, age, name = 'unknown', parent_permmission: true)
     super(age, name, parent_permmission)
     @specialization = specialization
